a9654440543444f91866f4d9c47edbf25329a220
balajiradhakrb <109693799+balajiradhakrb@users.noreply.github.com>
2023-03-15 13:31:41 -0300

    Update installhttpd.sh
    Made changes to code

--- installhttpd.sh
+++ installhttpd.sh
@@ -4,4 +4,4 @@ yum update -y
 yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
-echo "Hello World by Ranjan Gnanasekaran from $(hostname -f)" > /var/www/html/index.html
+echo "Hello World by Balaji Radhakrishnan from $(hostname -f)" > /var/www/html/index.html
