025f3481fa49b15d4a9f9c6f86def7a387ee83ee
balajiradhakrb <109693799+balajiradhakrb@users.noreply.github.com>
2023-03-15 13:31:15 -0300

    Update main.tf
    Made changes to code

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ resource "aws_instance" "my-instance" {
   ami                    = var.AMIS[var.region]
   instance_type          = var.instance_type
   availability_zone      = var.zone1
-  key_name               = "terraform-key"
-  vpc_security_group_ids = ["sg-05b8da38930f49227"]
+  key_name               = "jenkins-key"
+  vpc_security_group_ids = ["sg-086febac7a5fc028f"]
   user_data              = filebase64("installhttpd.sh")
   tags = {
     Name = "terraform instance"
